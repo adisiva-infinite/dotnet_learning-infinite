05fd061294586392c3f402023a7e45c44863a3ec
Adisiva Pattapu <adisivap@infinite.com>
2024-11-17 21:53:28 +0530

    Assignment-6

---
+++ C_Sharp/Assignments/Assignment-6/Assignment-6/Assignment-6_ClassLibrary-/Assignment-6_ClassLibrary-/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6_ClassLibrary_
+{
+    public class Program
+    {
+        public const int totalfare = 500;
+        public static void Main(String[] args)
+        {
+            Console.Write("Enter the name : ");
+            String name = Console.ReadLine();
+            Console.Write("Enter the age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Class1 c1 = new Class1();
+            c1.CalculateConcession(age);
+            Console.Read();
+        }
+    }
+}
