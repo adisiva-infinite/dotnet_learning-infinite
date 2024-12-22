58c2b0b8308e7dba50a4f35f0c3d6ff613deb0bb
Adisiva Pattapu <adisivap@infinite.com>
2024-12-22 23:36:04 +0530

    ASP Assignment-1

---
+++ ASP/Training/WebApplication1/WebApplication1/Cookies_destination.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class DataForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lbldata1.Text = Request.Cookies["uname"].Value.ToString();
+            lbldata2.Text = Request.Cookies["pass"].Value.ToString();
+        }
+    }
+}
